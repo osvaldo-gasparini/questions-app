bba8b8bd291482faa35a710030858bd4345f2e54
osvaldo-gasparini <osvaldo@selehann.com>
2023-07-31 20:19:17 -0300

    style: change UI general

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -16,8 +16,8 @@ const Form = () => {
   };
 
   return (
-    <div className="flex flex-col items-center w-full mb-4 max-w-md gap-2">
-      <div className="flex flex-row justify-between w-full relative">
+    <div className="flex flex-row items-center justify-center w-full h-14 px-2 mb-4 max-w-md">
+      <div className="flex flex-row rounded-l-3xl h-full bg-white shadow-inner justify-between items-center relative">
         <input
           value={inputValue}
           name="question"
@@ -26,12 +26,12 @@ const Form = () => {
           onChange={(e) => {
             setInputValue(e.target.value);
           }}
-          className="w-full bg-white shadow-inner px-4 py-2 rounded-3xl border-none placeholder:text-gray-400 placeholder:text-center focus:placeholder:text-black-oil focus:outline-none"
+          className="w-full ml-2 border-none placeholder:flex placeholder:pl-2 placeholder:leading-[3rem] resize-none placeholder:text-gray-400 focus:placeholder:text-black-oil focus:outline-none"
         />
         <button
           className={`${
-            inputValue.length > 0 ? "block" : "hidden"
-          } text-lg absolute right-0 transition-all px-4`}
+            inputValue.length > 0 ? "visible" : "invisible"
+          } text-lg right-0 transition-all px-4`}
           onClick={() => {
             setInputValue("");
           }}
@@ -40,11 +40,11 @@ const Form = () => {
         </button>
       </div>
       <button
-        className="bg-black-oil text-white-steam shadow-inner rounded-3xl w-fit px-6 py-2"
+        className="bg-black-oil text-white-steam shadow-inner w-10 h-full rounded-r-3xl"
         type="submit"
         onClick={onSubmit}
       >
-        Send
+        <p className="rotate-90">▲</p>
       </button>
     </div>
   );
--- src/components/QuestionBox/index.tsx
+++ src/components/QuestionBox/index.tsx
@@ -15,33 +15,11 @@ const QuestionBox = ({ question }: { question: Question }) => {
   };
 
   return (
-    <div
-      className={`rounded-2xl p-4 h-full ${
-        question ? "bg-white" : "bg-gray-400"
-      }`}
-    >
-      {question ? (
-        <>
-          <p className="text-xl text-black font-semibold">{question.text}</p>
-          <div className="flex flex-row justify-between items-center">
-            <p className="text-gray-400 font-medium">{timeAgo()}</p>
-          </div>
-        </>
-      ) : (
-        <div className="animate-pulse flex space-x-4">
-          <div className="rounded-full bg-slate-700 h-10 w-10"></div>
-          <div className="flex-1 space-y-6 py-1">
-            <div className="h-2 bg-slate-700 rounded"></div>
-            <div className="space-y-3">
-              <div className="grid grid-cols-3 gap-4">
-                <div className="h-2 bg-slate-700 rounded col-span-2"></div>
-                <div className="h-2 bg-slate-700 rounded col-span-1"></div>
-              </div>
-              <div className="h-2 bg-slate-700 rounded"></div>
-            </div>
-          </div>
-        </div>
-      )}
+    <div className="rounded-2xl p-4 h-full flex flex-col justify-between bg-white">
+      <p className="text-xl text-black font-semibold">{question.text}</p>
+      <div className="flex flex-row justify-between items-center">
+        <p className="text-gray-400 font-medium">{timeAgo()}</p>
+      </div>
     </div>
   );
 };
